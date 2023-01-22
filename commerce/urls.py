0c91cc333224e743409ed22063ecfbfb6463dec2
Dado <grubor.it@gmail.com>
2023-01-22 18:39:02 +0100

    commerce urls changed

--- commerce/urls.py
+++ commerce/urls.py
@@ -1,6 +1,7 @@
-"""commerce URL Configuration
+"""
+Commerce URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
