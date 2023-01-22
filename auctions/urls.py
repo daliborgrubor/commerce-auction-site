2d5c175bb1e6ae6efe4018f6f5f5f0633befc7a4
Dado <grubor.it@gmail.com>
2023-01-22 18:15:37 +0100

    urls updated

--- auctions/urls.py
+++ auctions/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
@@ -15,4 +16,5 @@ urlpatterns = [
     path("add_bid/<int:id>", views.add_bid, name="add_bid"),
     path("close_auction/<int:id>", views.close_auction, name="close_auction"),
     path("add_comment/<int:id>", views.add_comment, name="add_comment")
+    
 ]
